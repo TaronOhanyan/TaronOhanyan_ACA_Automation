724ed9f0347266ceb0bd1194188b37e99a16e4eb
TaronOhanyan <107707671+TaronOhanyan@users.noreply.github.com>
2022-07-18 19:48:09 +0400

    Update SeleniumBase.java

--- Java/src/main/java/Base/SeleniumBase.java
+++ Java/src/main/java/Base/SeleniumBase.java
@@ -17,27 +17,6 @@ public class SeleniumBase {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
         ederfr
-//        driver.navigate().to("https://www.amazon.com/");
-//        driver.navigate().back();
-//        TimeUnit.SECONDS.sleep(1);
-//        String title = driver.getTitle();
-//        System.out.println(title);
-//
-//        //find the search field
-//        WebElement searchField = driver.findElement(By.id("twotabsearchtextbox"));
-//
-//        // input value into field
-//        searchField.sendKeys("macbook");
-//
-//        // find search button
-//        WebElement searchButton = driver.findElement(By.cssSelector("[id='nav-search-submit-text']"));
-//
-//        // click on search button
-//        searchButton.click();
-//
-//
-//        driver.close();
-
 
     }
 }
