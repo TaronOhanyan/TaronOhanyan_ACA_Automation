1cbdd5e7c0d4202927e4cedafbbc34a4ebbcb005
TaronOhanyan <107707671+TaronOhanyan@users.noreply.github.com>
2022-07-18 19:53:10 +0400

    Update SeleniumBase.java

--- Java/src/main/java/Base/SeleniumBase.java
+++ Java/src/main/java/Base/SeleniumBase.java
@@ -15,27 +15,16 @@ public class SeleniumBase {
     public static void amazonTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-        ederfr
-//        driver.navigate().to("https://www.amazon.com/");
-//        driver.navigate().back();
-//        TimeUnit.SECONDS.sleep(1);
-//        String title = driver.getTitle();
-//        System.out.println(title);
-//
-//        //find the search field
-//        WebElement searchField = driver.findElement(By.id("twotabsearchtextbox"));
-//
-//        // input value into field
-//        searchField.sendKeys("macbook");
-//
-//        // find search button
-//        WebElement searchButton = driver.findElement(By.cssSelector("[id='nav-search-submit-text']"));
-//
-//        // click on search button
-//        searchButton.click();
-//
-//
+        driver.get("https://courses.letskodeit.com/practice");
+
+
+        //find BMW RadioButton
+        WebElement BMWRadioButton = driver.findElement(By.id("id=\"bmwradio"));
+
+        // click on BMW RadioButton
+        BMWRadioButton.click();
+
+
 //        driver.close();
 
 
