02b6f9ad1f109da0eb17880f0e6542a77373e56e
TaronOhanyan <107707671+TaronOhanyan@users.noreply.github.com>
2022-07-18 19:36:59 +0400

    update

--- Java/src/main/java/Base/SeleniumBase.java
+++ Java/src/main/java/Base/SeleniumBase.java
@@ -15,27 +15,19 @@ public class SeleniumBase {
     public static void amazonTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-        ederfr
-//        driver.navigate().to("https://www.amazon.com/");
-//        driver.navigate().back();
-//        TimeUnit.SECONDS.sleep(1);
-//        String title = driver.getTitle();
-//        System.out.println(title);
-//
-//        //find the search field
-//        WebElement searchField = driver.findElement(By.id("twotabsearchtextbox"));
-//
-//        // input value into field
-//        searchField.sendKeys("macbook");
-//
-//        // find search button
-//        WebElement searchButton = driver.findElement(By.cssSelector("[id='nav-search-submit-text']"));
-//
-//        // click on search button
-//        searchButton.click();
-//
-//
+        driver.get("https://www.techlistic.com/p/demo-selenium-practice.html");
+
+        boolean value = driver.findElement(By.cssSelector("[style=\"border: 1px solid rgb(221, 221, 221); box-sizing: inherit; padding: 8px;\"]")).isDisplayed();
+        System.out.println(value);
+
+
+
+
+
+
+
+
+
 //        driver.close();
 
 
