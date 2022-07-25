93b1692a9823c724c5d5ab1ada6a70de6406c5fc
TaronOhanyan <107707671+TaronOhanyan@users.noreply.github.com>
2022-07-25 19:01:55 +0400

    Merge remote-tracking branch 'origin/15.07.2022' into 15.07.2022

--- Java/src/main/java/Base/SeleniumBase.java
+++ Java/src/main/java/Base/SeleniumBase.java
@@ -4,7 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 
+
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class SeleniumBase {
@@ -15,8 +21,24 @@ public class SeleniumBase {
     public static void amazonTest() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-        ederfr
+        driver.get("https://www.auto.am/");
+
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class=\"select2-selection select2-selection--single\"]")));
+
+        WebElement selectElement = driver.findElement(By.id("386"));
+        Select selectObject = new Select(selectElement);
+        selectObject.selectByValue("value1");
+
+
+
+
+
+
+
+
+
+
 //        driver.navigate().to("https://www.amazon.com/");
 //        driver.navigate().back();
 //        TimeUnit.SECONDS.sleep(1);
