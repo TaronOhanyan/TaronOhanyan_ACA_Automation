4b1728da071d36abae9b80f40ee76aac4509d26b
TaronOhanyan <107707671+TaronOhanyan@users.noreply.github.com>
2022-07-27 17:34:44 +0400

    Merge remote-tracking branch 'origin/main'

--- Java/src/main/java/Base/TestBase.java
+++ Java/src/main/java/Base/TestBase.java
@@ -6,23 +6,57 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class TestBase {
     @Test
     public void Test1() {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.demoblaze.com/");
+        driver.get("https://www.demoblaze.com/index.html");
 
         WebElement SignUp = driver.findElement(By.id("signin2"));
         SignUp.click();
 
+        // find Name
+        WebElement Name = driver.findElement(By.id("sign-username"));
 
-        WebElement UserName = driver.findElement(By.id("sign-username"));
-        UserName.click();
-
-        UserName.sendKeys("Taron");
+        // input Name field
+        Name.sendKeys("Taron");
 
 
+
+
+
+
+
+
+
+
+
+
+
+//        String window1 = driver.getWindowHandle();
+//
+//
+//
+//        String window2 = null;
+//
+//        for (String window : currentWindows) {
+//            if (!window.equals(window1)) {
+//                window2 = window;
+//            break;
+//            }
+//        }
+//
+//        driver.switchTo().window(window2);
+//        driver.get("https://www.demoblaze.com/index.html");
+
+
+
+//        WebElement UserName = driver.findElement(By.cssSelector("[class=\"form-control\"]"));
+//
+//        UserName.sendKeys("Taron");
 
 //        // find Name
 //        WebElement Name = driver.findElement(By.cssSelector("[name=\"name\"]"));
